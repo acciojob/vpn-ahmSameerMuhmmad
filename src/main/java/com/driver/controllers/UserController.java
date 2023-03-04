b23c168cc884aefee6514513cc9d1d99ea244c4e
MUHMMAD SAMEER🥀🌼 <muhmmadsameer143@gmail.com>
2023-03-04 22:37:02 +0530

    solution

--- src/main/java/com/driver/controllers/UserController.java
+++ src/main/java/com/driver/controllers/UserController.java
@@ -1,4 +1,5 @@
 package com.driver.controllers;
+import com.driver.model.User;
 import com.driver.services.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,9 @@ public class UserController {
         //create a user of given country. The originalIp of the user should be "countryCode.userId" and return the user. Note that right now user is not connected and thus connected would be false and maskedIp would be null
         //Note that the userId is created automatically by the repository layer
         User user = userService.register(username, password, countryName);
+
+
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
